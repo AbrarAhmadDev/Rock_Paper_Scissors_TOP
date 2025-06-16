f42e1759d9f073bfebb94914ab6e01dad39b10ec
AbrarAhmadDev <abrarahmad.workplace@gmail.com>
2025-06-16 22:44:05 +0500

    Added getHumanChoice function

--- Rock_Paper_Scissors_TOP.js
+++ Rock_Paper_Scissors_TOP.js
@@ -5,3 +5,12 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 console.log(getComputerChoice());
+// Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
+// Hint: Use the prompt method to get the user’s input.
+// You do not need to handle reprompting if the user enters an invalid input, as that would require things we will teach later. For now, just assume the user will always enter a valid choice.
+// Test what your function returns by using console.log.
+function getHumanChoice() {
+    const choice = prompt("Enter Rock, Paper, or Scissors: ");
+    return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+}
+console.log(getHumanChoice());
